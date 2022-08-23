e137c74ddb3ac00d0fd6f2d741ca8f64df087a2c
xstartstaticx <daniel.reiller@icloud.com>
2022-08-23 10:05:03 +0200

    finished exercise

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 // Write your code here:
+function fibonacci(n, counter = 1, arr = [0, 1]) {
+  if (n === 0) {
+    return [0];
+  } else if (n === 1) {
+    return [0, 1];
+  }
+  arr.push(arr[counter] + arr[counter - 1]);
 
+  counter++;
 
+  if (counter >= n) return arr;
+
+  return fibonacci(n, counter, arr);
+}
 
 console.log(fibonacci(0)); // [0]
 console.log(fibonacci(1)); // [ 0, 1 ]
